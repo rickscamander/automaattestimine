b9c3ffee7c6de740369cd08e93a0035aece39f6d
kristjan <kristjan@ASH>
2017-09-27 19:47:38 +0300

    algus

---
+++ weather/src/weather/WeatherInfoTest.java
@@ -0,0 +1,14 @@
+package weather;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WeatherInfoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
