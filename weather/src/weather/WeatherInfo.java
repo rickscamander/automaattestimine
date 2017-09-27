55cf2f23b862c0fc35ac3fb1ea133c81ad2f1842
kristjan <kristjan@ASH>
2017-09-27 20:22:31 +0300

    esimesed testid

--- weather/src/weather/WeatherInfo.java
+++ weather/src/weather/WeatherInfo.java
@@ -2,6 +2,12 @@ package weather;
 
 public class WeatherInfo {
 
+public static String currentTemperature;
+public static float predictedHigh;
+public static float predictedLow;
+public static String latitude;
+public static String longitude;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- weather/src/weather/WeatherInfoTest.java
+++ weather/src/weather/WeatherInfoTest.java
@@ -7,8 +7,27 @@ import org.junit.Test;
 public class WeatherInfoTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void currentTemperature() {
+		assertNotNull(WeatherInfo.currentTemperature);
 	}
-
+	
+	@Test
+	public void predictedHigh() {
+		assertNotNull(WeatherInfo.predictedHigh);
+	}
+	
+	@Test
+	public void predictedLow() {
+		assertNotNull(WeatherInfo.predictedLow);
+	}
+	
+	@Test
+	public void latitude() {
+		assertNotNull(WeatherInfo.latitude);
+	}
+	
+	@Test
+	public void longitude() {
+		assertNotNull(WeatherInfo.longitude);
+	}
 }
